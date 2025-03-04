3d70bc11388fda6634f90003819b692eae497cbd
Milos Nikolic - Webflow Specialist <info@milosn.me>
2025-03-05 00:39:24 +0300

    First commit

---
+++ class_1/script.js
@@ -0,0 +1,22 @@
+// Variables in JS,  we can use them to store data, they can change values
+// To create Variable we use let (prefix) name (name of variable) = "" (values for created variable, "" use only for array-text)
+
+
+let name = "Milos";
+let surname = "Nikolic";
+
+// To execute js, we need to connect .js file with .html. Add script inside the body tag
+// Always use CONSOLE in browser for js testing
+// For script testing in console, use instruction  console.log(); if everything is fine it will show values for created variables if we use variable name, or plain text instead
+
+
+console.log(name, surname);
+
+
+
+// Test 1.  Make two variables, myName and  age then use Console log to execute "Hi my name is "name", and i am "age" old.
+
+let myName = "Milos";
+let age = 36;
+
+console.log("Hi, my name is " + myName, "and i am " + age, "years old");  // Test successfully passed in Console
